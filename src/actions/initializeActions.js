5df3a8a7b102c8ed616e5208f4dc5662f64facad
MIKI90 <malejo35@outlook.com>
2016-03-31 13:38:20 -0600

    Flux
    se agregaron caracteristicas de flux.

---
+++ src/actions/initializeActions.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionTypes = require('../constants/actionTypes');
+var AuthorApi = require('../api/authorApi');
+
+
+var InitializeActions = {
+       initApp: function() {
+          Dispatcher.dispatch({
+            actionType: ActionTypes.INITIALIZE,
+            InitialData: {
+                authors: AuthorApi.getAllAuthors()
+            }
+          });
+       }
+};
+
+module.exports = InitializeActions;
--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -3,22 +3,30 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = require('react-router').Link;
-var AuthorApi = require('../../api/authorApi');
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
 var AuthorList = require('./authorList');
 
 var Authors = React.createClass({
 	getInitialState: function(){
 		return {
-               authors: []
+               authors: AuthorStore.getAllAuthors()
 			};
 	},
+  
+  componentWillMount: function(){
+     AuthorStore.addChangeListener(this._onChange);
+  },
 
-    componentDidMount: function(){
-	if(this.isMounted()){
-        this.setState({authors: AuthorApi.getAllAuthors()});
-      }
-	},
-     render: function() {
+   componentWillUnmount: function(){
+    AuthorStore.removeChangeListener(this._onChange);
+   },
+  
+  _onChange: function(){
+      this.setState({authors: AuthorStore.getAllAuthors()});
+  },
+
+  render: function() {
          return (
              <div>
                 <h1>Authors</h1>
